aece4bda10ee963b8f43c76e0e30670c161c7b7b
raissalotin <rahlotin@gmail.com>
2023-03-24 16:25:41 -0300

    l

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+const contador1 = ref(0)
+const contador2 = ref(0)
+</script>
+<template>
+  <div class="container">
+    <button class="btn btn-primary">OK</button>
+    <div class="mt-4">
+      <div>
+        <button class="btn btn-primary" @click="contador1--">-</button>
+        <span class="mx-3 fs-4">{{ contador1 }}</span>
+        <button class=" btn btn-primary" @click="contador1++">+</button>
+      </div>
+      <div>
+        <button class="btn btn-dark" @click="contador2--">-</button>
+        <span class="m-3 fs-4">{{ contador2 }}</span>
+        <button class="btn btn-dark" @click="contador2++">+</button>
+      </div>
+    </div>
+  </div>
+</template>
