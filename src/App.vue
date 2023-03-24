4d1dd668f88ccf4dd609d0526d3b046ed30c4e3e
raissalotin <rahlotin@gmail.com>
2023-03-24 16:40:44 -0300

    exercicios bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 const contador1 = ref(0)
 const contador2 = ref(0)
+const soma= computed(()=> contador1.value + contador2.value)
 </script>
 <template>
   <div class="container">
@@ -10,7 +11,7 @@ const contador2 = ref(0)
       <div>
         <button class="btn btn-primary" @click="contador1--">-</button>
         <span class="mx-3 fs-4">{{ contador1 }}</span>
-        <button class=" btn btn-primary" @click="contador1++">+</button>
+        <button class="btn btn-primary" @click="contador1++">+</button>
       </div>
       <div>
         <button class="btn btn-dark" @click="contador2--">-</button>
@@ -18,5 +19,11 @@ const contador2 = ref(0)
         <button class="btn btn-dark" @click="contador2++">+</button>
       </div>
     </div>
+    <div>
+      <p class="fs-4">a soma é {{ soma }}</p>
+      <p v-if = "soma> 10" class = "fs-4"> a soma é maior do que 10</p>
+      <p v-else class="fs-4">a soma é menor ou igual a 10</p>
+          </div>
   </div>
 </template>
+<style scoped></style>
